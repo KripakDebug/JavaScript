ae7daeaea0b9b99617230a40e97acaea20fbb368
KripakDebug <kastrula123@gmail.com>
2021-11-21 03:29:14 +0200

    Calc

--- js/script.js
+++ js/script.js
@@ -1,32 +1,26 @@
-// function question(arg) {
-//   arg = prompt("Скільки вам років?");
-//   if (arg <= 11) {
-//     alert("Ви являєтесь дитиною");
-//   }
-//   else if (arg < 18) {
-//     alert("Ви являєтесь підлітком");
-//   }
-//   else if (arg < 60) {
-//     alert("Ви являєтесь дорослим");
-//   }
-//   else if (arg <= 100){
-//     alert("Ви являєтесь пенсіонером ");
-//   }
-//   else if (arg >= 100){
-//     alert("Ви довгожитель вітаю!!!");
-//   }
-// }
-// question();
-
-// Дізнатись рік народження людини
-
-// const str = prompt ('Введіть трьохзначне число');
-// let text = 'Повторних цифр немає';
-// for (var i = 0; i < str.length; i++)
-//    {
-//    if (str.split (str [i]).length > 2) {text = 'в числі є повторні цифри'; break;}
-//    }
-// alert (text);
-
-// Найти збігання цифр
-
+function calc() {
+  const input = document.getElementById("input");
+  const btns = document.querySelectorAll(".btn");
+  const btnClear = document.querySelector(".btn-js");
+  const btnEqual = document.getElementById("num-equal");
+  const btnBack = document.querySelector('.back');
+  btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      input.value += btn.textContent;
+    });
+  });
+btnBack.addEventListener('click', () => {
+    let result = input.value;
+    input.value = result.substring(0, result.length-1);
+})
+  btnClear.addEventListener("click", () => {
+   input.value = ''
+  });
+  btnEqual.addEventListener("click", () => {
+    let result = input.value;
+    if(result){
+        input.value = eval(result);
+    }
+  });
+}
+calc();
